c1cde24f38e9caadf570f041eca3adab2feb1b42
Ashhad <ashhadcomputing2019@gmail.com>
2021-12-19 22:57:34 +0500

    Flutter Classes!

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber,
+        appBar: AppBar(
+          backgroundColor: Colors.cyan,
+          title: const Text(
+            'My App',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+          // boolean
+          leading: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black45,
+            size: 20,
+          ),
+
+          actions: const [
+            Icon(
+              Icons.shopping_cart,
+              color: Colors.black45,
+              size: 30,
+            ),
+          ],
+        ),
+        body: const Center(
+          child: Text(
+            'Hello there!',
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
